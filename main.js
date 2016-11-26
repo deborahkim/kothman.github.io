30e2a6f8ad1dd47cb1e3311d6d965e6d14432e23
Adrian Kothman <adrian@veerwest.com>
2016-11-26 15:30:26 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+(function () {
+	init();
+
+	function init() {
+		checkJQuery();
+		addEventListeners();	
+	}
+
+	function checkJQuery() {
+		if (!window.jQuery) {
+			console.log("jQuery is required.");
+		}
+	}
+
+	function addEventListeners() {
+		// Event listener for toggling navigation visible on mobile
+		$("#toggle").on("click", toggleNav);
+	}
+
+	function toggleNav() {
+		var nav = $(".nav-menu");
+		console.log(nav.css("display"));
+		nav.css("display") == "none" ?
+			nav.css("display", "block") :
+			nav.css("display", "");
+	}
+
+})();
